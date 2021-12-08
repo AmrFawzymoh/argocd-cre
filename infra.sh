484e0276377df81ec0947156329c378de217cf39
amr fawzy <amrfawzymoh@gmail.com>
2021-12-08 04:39:17 +0200

    deleting the newlines from the infra.sh bash scripting file

--- infra.sh
+++ infra.sh
@@ -1,7 +1,6 @@
 #!/bin/bash 
 
 cre_namespace=argocd-cre
-
 cp install-template.yaml install-$cre_namespace.yaml
 
 eval "cat <<EOF
@@ -10,5 +9,4 @@ EOF
 " 2> /dev/null > install-$cre_namespace.yaml
 
 kubectl create ns $cre_namespace
-
 kubectl -n $cre_namespace apply -f install-$cre_namespace.yaml
