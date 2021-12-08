6f98824b513cff38a53abd59750f03db4568431c
amr fawzy <amrfawzymoh@gmail.com>
2021-12-08 04:36:03 +0200

    Creating infra.sh bash scripting file to automate the deployment of ArgoCD CRE instance on the client's cluster, also Creating an install template file with the required adjustment for the new ArgoCD instance

---
+++ infra.sh
@@ -0,0 +1,14 @@
+#!/bin/bash 
+
+cre_namespace=argocd-cre
+
+cp install-template.yaml install-$cre_namespace.yaml
+
+eval "cat <<EOF
+$(<install-$cre_namespace.yaml)
+EOF
+" 2> /dev/null > install-$cre_namespace.yaml
+
+kubectl create ns $cre_namespace
+
+kubectl -n $cre_namespace apply -f install-$cre_namespace.yaml
