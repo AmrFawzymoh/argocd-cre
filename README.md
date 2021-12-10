34878bdd14a8898201c26442022b46686760e25d
amr fawzy <amrfawzymoh@gmail.com>
2021-12-11 00:39:10 +0200

    updating the README.md to add step asking to modify the namespace value, update the deploy script to use more generic namespace variable, update the install.yaml script to use the new variable, update the argocd app manifest to remove the hardcoded namespace value

--- README.md
+++ README.md
@@ -15,4 +15,5 @@ The client may already has his own ArgoCD instance running on his cluster, so We
 * Copy the argocd-application-template.yaml to new file argocd-application.yaml and modify it with your GitHub repo URL and manifests path.
 * Generate a GitHub access token with repo scopes
 * Copy the repo-access-token-template.yaml to new file repo-access-token.yaml and modify it with your newly generated github access token
+* Modify the deploy-argocd-cre.sh script to set the value of the argocd_namespace variable to your desired namespace
 * Run the deploy-argocd-cre.sh bash script to deploy the ArgoCD instance for the CRE team and to create a test application that conclude the GitOps workflow.
--- deploy-argocd-cre.sh
+++ deploy-argocd-cre.sh
@@ -1,22 +1,22 @@
 #!/bin/bash 
 
 # Declare the namespace used to deploy the ArgoCD instance for the CRE team on the client cluster
-cre_argocd_namespace=argocd-cre
+argocd_namespace=argocd-client
 
 # Create a modified install.yaml file specifing the new namespace and the two new ClusterRoleBinding
-cp install-template.yaml install-$cre_argocd_namespace.yaml
+cp install-template.yaml install-$argocd_namespace.yaml
 
 eval "cat <<EOF
-$(<install-$cre_argocd_namespace.yaml)
+$(<install-$argocd_namespace.yaml)
 EOF
-" 2> /dev/null > install-$cre_argocd_namespace.yaml
+" 2> /dev/null > install-$argocd_namespace.yaml
 
 # Deploy the ArgoCD instance for the CRE team in the new namespace using the modified install manifest
-kubectl create ns $cre_argocd_namespace
-kubectl -n $cre_argocd_namespace apply -f install-$cre_argocd_namespace.yaml
+kubectl create ns $argocd_namespace
+kubectl -n $argocd_namespace apply -f install-$argocd_namespace.yaml
 
 # Create a K8s secret for the GitHub access token used to grant the ArgoCD instance access to the CRE GitHub repository
-kubectl -n $cre_argocd_namespace apply -f repo-access-token.yaml
+kubectl -n $argocd_namespace apply -f repo-access-token.yaml
 
 # Create ArgoCD application
-kubectl -n $cre_argocd_namespace apply -f argocd-application.yaml
+kubectl -n $argocd_namespace apply -f argocd-application.yaml
